c016a13bbc13bf9aac466797de34bf8ec4eb5648
softprops <d.tangren@gmail.com>
2012-02-08 23:39:35 -0500

    updated scripted tests

---
+++ src/sbt-test/less-sbt/custom-target/build.sbt
@@ -0,0 +1,3 @@
+seq(lessSettings: _*)
+
+resourceManaged in (Compile, LessKeys.less) <<= resourceManaged in Compile
---
+++ src/sbt-test/less-sbt/custom-target/src/main/scala/app.scala
@@ -0,0 +1,3 @@
+// we need at least compile scala compilation
+// unit to generate the classes dir under target
+object App
