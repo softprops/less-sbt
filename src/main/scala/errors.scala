cd8885d58bafd1382818c139dca104f2a1566861
softprops <d.tangren@gmail.com>
2012-03-17 00:39:22 -0400

    checkpoint

--- src/main/scala/errors.scala
+++ src/main/scala/errors.scala
@@ -19,7 +19,7 @@ object LessError {
       props("line").toString.toInt,
       props("column").toString.toInt,
       props("message").toString,
-      props("extract").asInstanceOf[Seq[Any]]
+      props("extract").asInstanceOf[Seq[String]]
     ) else if(props.isDefinedAt("message")) {
       UndefVar.findFirstMatchIn(props("message").toString) match {
         case Some(mat) =>
@@ -27,7 +27,7 @@ object LessError {
             mat.group(1),
             props("line").toString.toInt,
             props("column").toString.toInt,
-            props("extract").asInstanceOf[Seq[Any]]
+            props("extract").asInstanceOf[Seq[String]]
           )
         case _ => GenericLessError(props)
       }
@@ -35,32 +35,32 @@ object LessError {
 }
 
 trait Extracts {
-  def showExtract(line: Int, col: Int, extract: Seq[Any]) =
-    extract match {
-      case Seq(null, at, after) =>
-        "\n >%3d| %s\n  %3d| %s".format(
+  def showExtract(line: Int, col: Int, extract: Seq[String]) =
+    (extract.size.toString.size, extract) match {
+      case (pad, Seq(null, at, after)) =>
+        "\n >%" + pad + "d| %s\n  %" + pad + "d| %s".format(
           line, at, line + 1, after
         )
-      case Seq(before, at, null) =>
-        "\n  %3d| %s\n> %3d| %s".format(
+      case (pad, Seq(before, at, null)) =>
+        "\n  %" + pad + "d| %s\n> %" + pad + "d| %s".format(
           line - 1, before, line, at
         )
-      case Seq(before, at, after) =>
-        "\n  %3d| %s\n> %3d| %s\n  %3d| %s".format(
+      case (pad, Seq(before, at, after)) =>
+        "\n  %" + pad + "d| %s\n> %" + pad + "d| %s\n  %" + pad + "d| %s".format(
           line - 1, before, line, at, line + 1, after
         )
       case ext => ext.mkString("\n | ", " | %\n", "")
     }
 }
 
-case class ParseError(line: Int, column: Int, message: String, extract: Seq[Any])
+case class ParseError(line: Int, column: Int, message: String, extract: Seq[String])
 extends CompilationError with Extracts {
   override def getMessage = "Parse error on line: %s, column: %s%s" format(
     line, column, showExtract(line, column, extract)
   )
 }
 
-case class UndefinedVar(name: String, line: Int, column: Int, extract: Seq[Any])
+case class UndefinedVar(name: String, line: Int, column: Int, extract: Seq[String])
 extends CompilationError with Extracts {
   override def getMessage = "Undefined variable %s on line: %s, column: %s%s" format(
     name, line, column, showExtract(line, column, extract)
